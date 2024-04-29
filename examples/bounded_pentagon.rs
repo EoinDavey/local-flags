3133dee30a43b0d7ec4182586d2a3eb141e8fe17
Eoin Davey <eoind@vey.ie>
2024-04-29 14:51:00 +0200

    Add size of X bound

--- examples/bounded_pentagon.rs
+++ examples/bounded_pentagon.rs
@@ -5,6 +5,8 @@ use flag_algebra::*;
 use itertools::{equal, Itertools, iproduct};
 use local_flags::Degree;
 
+use num::pow::Pow;
+
 type G = Colored<Graph, 2>;
 #[derive(Debug, Clone, Copy)]
 pub enum TriangleFreeConnected {}
@@ -42,26 +44,46 @@ impl SubFlag<G> for TriangleFreeConnected {
     }
 }
 
+// Sum of flags with type `t` and size `t.size + 1` where the extra vertex is in X
+fn extension_in_x(t: Type<F>) -> V {
+    let b = Basis::new(t.size + 1).with_type(t);
+    b.qflag_from_indicator(|g: &F, type_size| g.content.color[type_size] == 0)
+        .named(format!("ext_in_x({{{}}})", t.print_concise()))
+}
+
+// Equalities expressing that extensions in X have twice the weight of extensions through an edge
+fn size_of_x(n: usize) -> Vec<Ineq<N, F>> {
+    let mut res = Vec::new();
+    for t in Type::types_with_size(n - 1) {
+        let diff = Degree::extension(t, 0) - extension_in_x(t);
+        res.push(diff.equal(0.).untype());
+    }
+    res
+}
+
 fn ones(n: usize, k: usize) -> V {
     Degree::project(&Colored::new(Graph::empty(k), vec![0; k]).into(), n)
 }
 
 pub fn main() {
     init_default_log();
-    let n = 6;
+    let n = 5;
     let basis = Basis::new(n);
 
-    let c5_path: F = Colored::new(Graph::new(4, &[(0, 1), (1, 2), (2, 3)]), vec![0, 1, 1, 0]).into();
+    //let c5_path: F = Colored::new(Graph::new(4, &[(0, 1), (1, 2), (2, 3)]), vec![0, 1, 1, 0]).into();
+    let c5_path: F = Colored::new(Graph::new(3, &[(0, 1)]), vec![0, 1, 0]).into();
+
     let new_obj = Degree::project(&c5_path, n).untype();
 
     let mut ineqs = vec![
         flags_are_nonnegative(basis),
     ];
-    for i in 1..n {
-        ineqs.push(ones(n, i).untype().equal(1.))
+    for i in 1..=n {
+        ineqs.push(ones(n, i).untype().equal(1.));
     }
 
     ineqs.append(&mut Degree::regularity(basis));
+    ineqs.append(&mut size_of_x(n));
 
     let pb = Problem::<N, _> {
         ineqs,
